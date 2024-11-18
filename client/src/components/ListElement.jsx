f658061dd2c15a49a0c941338516e8d0c6d448cd
Jatin Singh <singh.jatin609@gmail.com>
2024-11-18 16:49:18 +0530

    done

--- client/src/components/ListElement.jsx
+++ client/src/components/ListElement.jsx
@@ -2,14 +2,13 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const ListElement = ({ elementData }) => {
-
   const navigate = useNavigate();
 
   async function handleDelete(uuid) {
-    await fetch(`http://localhost:8000/api/admins/${uuid}`,{
-      method: 'delete',
-    }).then((res)=>{
-      if(res.ok) {
+    await fetch(`http://localhost:8000/api/admins/${uuid}`, {
+      method: "delete",
+    }).then((res) => {
+      if (res.ok) {
         navigate(0);
       }
     });
@@ -17,13 +16,24 @@ const ListElement = ({ elementData }) => {
 
   return (
     <div className="ListElement">
-      <div><span>{elementData.Name}</span></div>
-      <div><span>{elementData.email}</span></div>
-      <div><span>{elementData.contact}</span></div>
+      <div>
+        <span>{elementData.Name}</span>
+      </div>
+      <div>
+        <span>{elementData.email}</span>
+      </div>
+      <div>
+        <span>{elementData.contact}</span>
+      </div>
       <div className="buttonSection">
-        <div className="EditButton" onClick={()=>{
-          navigate(`/update/${elementData.uuid}`)
-        }}>Edit</div>
+        <div
+          className="EditButton"
+          onClick={() => {
+            navigate(`/update/${elementData.uuid}`);
+          }}
+        >
+          Edit
+        </div>
         <div
           className="EditButton"
           onClick={() => {
@@ -32,7 +42,16 @@ const ListElement = ({ elementData }) => {
         >
           View
         </div>
-        <div className="EditButton" style={{ backgroundColor: "red" }} onClick={()=>{handleDelete(elementData.uuid)}}>
+        <div
+          className="EditButton"
+          style={{ backgroundColor: "red" }}
+          onClick={() => {
+            const a = confirm("Are you sure? Delete action cannot be undone");
+            if (a) {
+              handleDelete(elementData.uuid);
+            }
+          }}
+        >
           Delete
         </div>
       </div>
